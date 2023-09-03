04169ca79fcc267192a7fa4ea1c95560e4aee428
Diego Hernández <108566267+diegocastilloho@users.noreply.github.com>
2023-09-03 11:15:09 -0600

    Creation HelloUPIICSA.kt
    1. Imprimir el mismo mensaje que imprimimos en la practica "HelloUPIICSA"
    
    2. Abajo de la linea de código de la tarea:
    a) Poner otro mensaje en consola ¿Qué pasa con los dos mensajes? Al utilizar "print" todo se imprime en la misma línea y no se crean espacios para apreciar bien las impresiones.
    
    3. Ahora utiliza println e imprime alguna otra cosa, ¿Qué diferencia hay con el print? Con "println" automáticamente se crea el primer salto de línea, sin embargo en mi caso el primer println no respetó el salto de línea y se imprimió junto con los otros 2 prints y eso me sorprendió, el segundo println si realizó el salto de línea.

---
+++ HelloUPIICSA.kt
@@ -0,0 +1,9 @@
+/**
+* You can edit, run, and share this code.
+* play.kotlinlang.org
+*/
+fun main() {
+    print("¡Hola UPIICSA!")
+    print("Diego Hernandez")
+    println("Imprimiendo en println")
+    println("Otro println")
