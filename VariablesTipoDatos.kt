a90b095a8572f4f6a2ab2a98d1bd36b7adb6a42c
Diego Hernández <108566267+diegocastilloho@users.noreply.github.com>
2023-09-10 21:48:26 -0600

    Create VariablesTipoDatos.kt
    Practica para comprobar ciertos tipos de datos asignados por defecto, uso de operador multiplicación y String Template.

---
+++ VariablesTipoDatos.kt
@@ -0,0 +1,29 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    
+    //1. Tipo de dato asignado a un numero decimal 
+    
+    var numDecimal = 1.56;
+    println(numDecimal);
+    
+    
+    //2. Conversión de variable decimal a flotante
+    
+    var floatDecimal = 1.56f;
+    println(floatDecimal);
+    
+    
+    //3. Const Val PI, multiplicacion y String Template
+    
+    val PI= 3.1416;
+    
+    val perimetro= PI * numDecimal;
+    
+    val StringPer="El perimetro del circulo es $perimetro";
+    
+    println(StringPer);
+    
+}
