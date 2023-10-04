a421991b8d274460b16d65a897971de3e5cb7334
Diego Hernández <108566267+diegocastilloho@users.noreply.github.com>
2023-10-03 23:18:48 -0600

    Create PerimetroCirculo.kt

---
+++ PerimetroCirculo.kt
@@ -0,0 +1,10 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+const val PI = 3.1416
+fun main() {
+    val decimal = 1.325F
+    val perimetro = 2*PI*decimal
+    println("El perimetro del circulo es: $perimetro")
+}
