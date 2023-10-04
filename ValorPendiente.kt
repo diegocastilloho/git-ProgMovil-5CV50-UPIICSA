f2711a0ad7243adb5f0e897e9615efb64a78764a
Diego Hernández <108566267+diegocastilloho@users.noreply.github.com>
2023-10-03 23:18:04 -0600

    Create ValorPendiente.kt

---
+++ ValorPendiente.kt
@@ -0,0 +1,13 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+fun main() {
+val y2 = -2f;
+val y1 = 3f;
+val x2 = -3f;
+val x1 = 4f;
+    val m = (y2-y1)/(x2-x1);
+    println(m)
+}
